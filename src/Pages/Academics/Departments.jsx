379c1273b67948e65be3c6d3e4a904cf1a47b4b2
IronFist396 <atharvroday396@gmail.com>
2025-06-12 23:42:41 +0530

    add department pages

--- src/Pages/Academics/Departments.jsx
+++ src/Pages/Academics/Departments.jsx
@@ -1,7 +1,42 @@
 import Page from "../../Components/BlogPage/Page";
 import navData from "../sideNavData.json";
+import Aero from './icons/Aero.svg';
+import Chemical from './icons/Chemical.svg';
+import Chemistry from './icons/Chemistry.svg';
+import Civil from './icons/civil.svg';
+import CSE from './icons/CSE.svg';
+import Eco from './icons/Eco.svg';
+import Elec from './icons/Elec.svg';
+import Energy from './icons/Energy.svg';
+import Ep from './icons/Ep.svg';
+import Enviro from './icons/Enviro.svg';
+import IDC from './icons/IDC.svg';
+import IEOR from './icons/IEOR.svg';
+import Maths from './icons/Maths.svg';
+import Mech from './icons/Mech.svg';
+import Meta from './icons/Meta.svg';
+
+import { Link } from "react-router-dom";
 
 const Departments = () => {
+  const departments = [
+  { name: "Aerospace Engineering", src: Aero, path: "aerospace" },
+  { name: "Chemical Engineering", src: Chemical, path: "chemical" },
+  { name: "Chemistry", src: Chemistry, path: "chemistry" },
+  { name: "Civil Engineering", src: Civil, path: "civil" },
+  { name: "Computer Science", src: CSE, path: "cse" },
+  { name: "Economics", src: Eco, path: "economics" },
+  { name: "Electrical Engineering", src: Elec, path: "electrical" },
+  { name: "Energy Science and Engineering", src: Energy, path: "energy" },
+  { name: "Engineering Physics", src: Ep, path: "engineering-physics" },
+  { name: "Environmental Sciences", src: Enviro, path: "environmental" },
+  { name: "Industrial Design Centre", src: IDC, path: "idc" },
+  { name: "IEOR", src: IEOR, path: "ieor" },
+  { name: "Mathematics", src: Maths, path: "maths" },
+  { name: "Mechanical Engineering", src: Mech, path: "mechanical" },
+  { name: "Metallurgical Engineering and Materials Science", src: Meta, path: "metallurgy" },
+];
+
   const content = (
     <>
       <h1 className="text-4xl font-bold mb-5">Departments</h1>
@@ -20,7 +55,21 @@ const Departments = () => {
       <p className="mb-4">
         <b>Humble Appeal :</b> Many students have fallen bait to the practice of blindly following the cutoffs of the previous years and have ended up disliking the branch that they choose. We hope YOU, the future IITian will make judicious use of this initiative and make an informed choice!
       </p>
-      {/* Incomplete Department Pages! */}
+      
+      {/* Department Grid Starts Here */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-10">
+        {departments.map((dept, index) => (
+          <Link
+            to={`/academics/departments/${dept.path}`}
+            key={index}
+            className="flex flex-col items-center text-center p-4 rounded-[10px] hover:shadow-lg transition-shadow duration-300 cursor-pointer"
+          >
+            <img src={dept.src} alt={dept.name} className="w-24 h-24 mb-4 object-contain" />
+            <p className="font-medium">{dept.name}</p>
+          </Link>
+        ))}
+      </div>
+
     </>
   )
   const sidebar = navData["Academics"];
