f813532330768e702a6462bbb6d78c591d83099b
Abdiitb <aryan223653badkul@gmail.com>
2025-06-14 14:03:25 +0530

    scholarship page content updated

--- src/Components/ScholarshipTable/ScholarshipTable.jsx
+++ src/Components/ScholarshipTable/ScholarshipTable.jsx
@@ -1,101 +1,122 @@
-import {Table} from '@radix-ui/themes'
+import { Table } from '@radix-ui/themes'
 
 const ScholarshipTable = () => {
   return (
     <Table.Root variant='surface' size="3">
-  <Table.Header>
-    <Table.Row>
-      <Table.ColumnHeaderCell>Income Limit</Table.ColumnHeaderCell>
-      <Table.ColumnHeaderCell>Women</Table.ColumnHeaderCell>
-      <Table.ColumnHeaderCell>Others</Table.ColumnHeaderCell>
-      <Table.ColumnHeaderCell>SC/ST Women</Table.ColumnHeaderCell>
-      <Table.ColumnHeaderCell>SC/ST Others</Table.ColumnHeaderCell>
-    </Table.Row>
-  </Table.Header>
+      <Table.Header>
+        <Table.Row>
+          <Table.ColumnHeaderCell>Income Limit</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Women</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>Others</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>SC/ST Women</Table.ColumnHeaderCell>
+          <Table.ColumnHeaderCell>SC/ST Others</Table.ColumnHeaderCell>
+        </Table.Row>
+      </Table.Header>
 
-  <Table.Body>
-    <Table.Row>
-      <Table.RowHeaderCell>Income Upto Rs. 1L</Table.RowHeaderCell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        MCM Rs. 40,000 per year<br />
-        Women merit scholarship Rs. 10,000 per year
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        MCM Rs. 40,000 per year
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        Women merit scholarship Rs. 10,000 per year<br /><br />
-        Free messing<br />
-        Pocket allowance @ 500 p.m<br />
-        Hostel rent exempted
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br /><br />
-        Free messing<br />
-        Pocket allowance @ 500 p.m<br />
-        Hostel rent exempted
-      </Table.Cell>
-    </Table.Row>
+      <Table.Body>
+        <Table.Row>
+          <Table.RowHeaderCell>Income Upto Rs. 1L</Table.RowHeaderCell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>MCM Rs. 40,000 per year</li>
+              <li>Women merit scholarship Rs. 10,000 per year</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>MCM Rs. 40,000 per year</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>Women merit scholarship Rs. 10,000 per year</li>
+              <li>Free messing</li>
+              <li>Pocket allowance @ 500 p.m</li>
+              <li>Hostel rent exempted</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>Free messing</li>
+              <li>Pocket allowance @ 500 p.m</li>
+              <li>Hostel rent exempted</li>
+            </ul>
+          </Table.Cell>
+        </Table.Row>
 
-    <Table.Row>
-      <Table.RowHeaderCell>Income above Rs. 1L to 3L</Table.RowHeaderCell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        MCM Rs. 30,000 per year<br />
-        Women merit scholarship Rs. 10,000 per year
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        MCM Rs. 30,000 per year
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        Women merit scholarship Rs. 10,000 per year<br /><br />
-        Free messing<br />
-        Pocket allowance @ 500 p.m<br />
-        Hostel rent exempted
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br /><br />
-        Free messing<br />
-        Pocket allowance @ 500 p.m<br />
-        Hostel rent exempted
-      </Table.Cell>
-    </Table.Row>
+        <Table.Row>
+          <Table.RowHeaderCell>Income above Rs. 1L to 3L</Table.RowHeaderCell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>MCM Rs. 30,000 per year</li>
+              <li>Women merit scholarship Rs. 10,000 per year</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>MCM Rs. 30,000 per year</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>Women merit scholarship Rs. 10,000 per year</li>
+              <li>Free messing</li>
+              <li>Pocket allowance @ 500 p.m</li>
+              <li>Hostel rent exempted</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>Free messing</li>
+              <li>Pocket allowance @ 500 p.m</li>
+              <li>Hostel rent exempted</li>
+            </ul>
+          </Table.Cell>
+        </Table.Row>
 
-    <Table.Row>
-      <Table.RowHeaderCell>Income above Rs. 3L upto 5L</Table.RowHeaderCell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        MCM Rs. 20,000 per year<br />
-        Women merit scholarship Rs. 10,000 per year
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        MCM Rs. 20,000 per year
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br />
-        Women merit scholarship Rs. 10,000 per year<br /><br />
-        Free messing<br />
-        Pocket allowance @ 500 p.m<br />
-        Hostel rent exempted
-      </Table.Cell>
-      <Table.Cell>
-        Exemption from tuition fees<br /><br />
-        Free messing<br />
-        Pocket allowance @ 500 p.m<br />
-        Hostel rent exempted
-      </Table.Cell>
-    </Table.Row>
-  </Table.Body>
-</Table.Root>
-
-
-    // </Container>
+        <Table.Row>
+          <Table.RowHeaderCell>Income above Rs. 3L upto 5L</Table.RowHeaderCell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>MCM Rs. 20,000 per year</li>
+              <li>Women merit scholarship Rs. 10,000 per year</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>MCM Rs. 20,000 per year</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>Women merit scholarship Rs. 10,000 per year</li>
+              <li>Free messing</li>
+              <li>Pocket allowance @ 500 p.m</li>
+              <li>Hostel rent exempted</li>
+            </ul>
+          </Table.Cell>
+          <Table.Cell>
+            <ul className='list-disc'>
+              <li>Exemption from tuition fees</li>
+              <li>Free messing</li>
+              <li>Pocket allowance @ 500 p.m</li>
+              <li>Hostel rent exempted</li>
+            </ul>
+          </Table.Cell>
+        </Table.Row>
+      </Table.Body>
+    </Table.Root>
   );
 };
 
--- src/Pages/IncomingStudents/Scholarships.jsx
+++ src/Pages/IncomingStudents/Scholarships.jsx
@@ -2,135 +2,135 @@ import Page from "../../Components/BlogPage/Page";
 import navData from "../sideNavData.json";
 import { Link } from "react-router-dom";
 import ScholarshipTable from "../../Components/ScholarshipTable/ScholarshipTable";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilePdf } from "@fortawesome/free-solid-svg-icons"
 
 const Scholarships = () => {
     const content = (<>
-    <h1 className="text-4xl font-bold mb-5">Scholarships Available</h1>
-    <h2 className="text-xl font-bold mb-2">
-       Means-Cum-Means Scholarship (MCM) 
-    </h2>
-    <p className="mb-4">
-        The parental income is the basis for the ‘means' criterion of the scholarship. At present students whose parents annual gross income from all sources for the previous Financial year does not exceed Rs. 5,00,000/ , are eligible to apply for Merit-­cum­-Means Scholarship.
-The student with a CPI of 6.00 and above only are eligible to apply for Merits-cum Means Scholarship (MCM).
-    </p>
-    <h2 className="text-xl font-bold mb-2">
-        Facilities of Free messing (For SC/ST students only)
-    </h2>
-    <p className="mb-4">
-        All SC/ST students are exempted from the payment of tuition fees. The facilities of free messing will be granted to eligible SC/ST students of B.Tech., Dual Degee (B.Tech. + M.Tech.), B.S. and B.Des. Programme as detailed below. For Dual Degee students, it is available only upto 4th year.
-    </p>
-    <p className="mb-4">
-        The parental income is the basis for the means criterion of the scholarship. At present, students whose parents’ annual gross income from all sources for the previous Financial year does not exceed Rs. 5,00,000 are eligible to apply for the above facilities.
-    </p>
+        <h1 className="text-4xl font-bold mb-5">Scholarships Available</h1>
+        <h2 className="text-xl font-bold mb-2">
+            Means-Cum-Means Scholarship (MCM)
+        </h2>
+        <p className="mb-4">
+            For these scholarships, academic merit as well as means criterion is taken into
+            account to determine eligibility. The parental income is the basis for the ‘means' criterion of the scholarship. At present students whose parents annual gross income from all sources for the previous Financial year does not exceed Rs. 5,00,000/ , are eligible to apply for Merit-­cum­-Means Scholarship.
+        </p>
+        <h2 className="text-xl font-bold mb-2">
+            Facilities of Free messing (For SC/ST students only)
+        </h2>
+        <p className="mb-4">
+            All SC/ST students are exempted from the payment of tuition fees. The facilities of free messing will be granted to eligible SC/ST students of B.Tech., Dual Degee (B.Tech. + M.Tech.), B.S. and B.Des. Programme as detailed below. For Dual Degee students, it is available only upto 4th year.
+        </p>
+        <p className="mb-4">
+            The parental income is the basis for the means criterion of the scholarship. At present, students whose parents’ annual gross income from all sources for the previous Financial year does not exceed Rs. 5,00,000 are eligible to apply for the above facilities.
+        </p>
 
-    <h2 className="text-xl font-bold mb-2">
-        Remission of Fees
-    </h2>
-    <p className="mb-4">
-        Remission of fees will be granted to eligible students of B.Tech, Dual Degee (B.Tech + M.Tech), B.S. and B.Des from 2016 Batch onwards.
-The students whose Gross Income of parents for the previous Financial year is not more than Rs. 5,00,000/­ per annum (without any standard deduction) are eligible to apply for Remission of fees.
-    </p>
-    <p className="mb-4">
-        The economically backward students are eligible for remission of tuition fee as follows:<br />
-1. Those with a parental income less than Rs. 1 Lakh per annum shall get full remission of the tuition fees. <br />
-2. Those with a parental income between Rs.1 Lakh and Rs. 5 Lakhs per annum shall get remission of 2/3rd of the tuition fees.
-    </p>
+        <h2 className="text-xl font-bold mb-2">
+            Remission of Fees
+        </h2>
+        <p className="mb-4">
+            Remission of fees will be granted to eligible students of B.Tech, Dual Degee (B.Tech + M.Tech), B.S. and B.Des from 2016 Batch onwards.
+            The students whose Gross Income of parents for the previous Financial year is not more than Rs. 5,00,000/­ per annum (without any standard deduction) are eligible to apply for Remission of fees.
+        </p>
+        <p className="mb-4">
+            The economically backward students are eligible for remission of tuition fee as follows:<br />
+            1. Those with a parental income less than Rs. 1 Lakh per annum shall get full remission of the tuition fees. <br />
+            2. Those with a parental income between Rs.1 Lakh and Rs. 5 Lakhs per annum shall get remission of 2/3rd of the tuition fees.
+        </p>
 
-    <h2 className="text-xl font-bold mb-2">
-        Scholarship Given
-    </h2>
-    <ScholarshipTable />
-    <p className="mt-8 mb-4">
-        <b>Please Note:</b><br />
-The benefits are available for the standard duration of the program on the registration and satisfactory progress. It will be limited to 25% of the admitted students in each batch.
-Pocket allowance is only for 10 months only in each academic year.
-Tuition fee is waived off for SC/ST/PD category students.<br />
-<b>In case of any query or clarification, contact person in-charge of UG Scholarships in Academic Office
-(2nd floor, main Building).</b>
-    </p>
-    
-    <h2 className="text-xl font-bold mb-2">
-        Named Scholarship Portal
-    </h2>
-    <p className="mb-4">
-       Named Scholarships­ Office of the Dean Alumni & Corporate Relations manages the Named scholarship endowed by the alumni of the Institute and other wellwishers. These Scholarships are awarded based on the following eligibility criteria :­
-       
-    </p>
+        <h2 className="text-xl font-bold mb-2">
+            Scholarship Given
+        </h2>
+        <ScholarshipTable />
+        <p className="mt-8 mb-4">
+            <b>Please Note:</b><br />
+            The benefits are available for the standard duration of the program on the registration and satisfactory progress. It will be limited to 25% of the admitted students in each batch.
+            Pocket allowance is only for 10 months only in each academic year.
+            Tuition fee is waived off for SC/ST/PD category students.<br />
+            <b>In case of any query or clarification, contact person in-charge of UG Scholarships in Academic Office
+                (2nd floor, main Building).</b>
+        </p>
+
+        <h2 className="text-xl font-bold mb-2">
+            Named Scholarship Portal
+        </h2>
+        <p className="mb-4">
+            Named Scholarships­ Office of the Dean Alumni & Corporate Relations manages the Named scholarship endowed by the alumni of the Institute and other wellwishers. These Scholarships are awarded based on the following eligibility criteria :­
+
+        </p>
         <ul className="list-disc ml-4 mb-4">
-        <li>
-        <b>Merit Criterion:</b> For new entrants of B.Tech., Dual Degee (B.Tech. + M.Tech.), B.S. their JEE AIR and for B.Des. their UCEED Rank, will be the criterion. In subsequent years, the criterion will be their academic performance during the preceding academic year.</li>
-        <li><b>Means Criterion: </b>The parental income is the basis for the ‘means' criterion of the scholarship. At present students whose parents annual gross income from all sources for the previous Financial year does not exceed Rs. 5,00,000/ , are eligible to apply for Merit­cum­Means Scholarship.</li>
-       </ul>
+            <li>
+                <b>Merit Criterion:</b> For new entrants of B.Tech., Dual Degee (B.Tech. + M.Tech.), B.S. their JEE AIR and for B.Des. their UCEED Rank, will be the criterion. In subsequent years, the criterion will be their academic performance during the preceding academic year.</li>
+            <li><b>Means Criterion: </b>The parental income is the basis for the ‘means' criterion of the scholarship. At present students whose parents annual gross income from all sources for the previous Financial year does not exceed Rs. 5,00,000/ , are eligible to apply for Merit­cum­Means Scholarship.</li>
+        </ul>
 
-       <p className="mb-4">
-        For complete information about the scholarships available, please visit <Link to="https://www.iitb.ac.in/newacadhome/scholar.jsp" target="_blank" className="text-blue-600 hover:underline p-0!">this page</Link>.
-       <br />
-        In case of any queries, please send email on <Link to="mailto:acrscholarship@iitb.ac.in" target="_blank" className="text-blue-600 hover:underline p-0!">acrscholarship@iitb.ac.in</Link>.
-       </p>
+        <p className="mb-4">
+            For complete information about the scholarships available, please visit <Link to="https://www.iitb.ac.in/newacadhome/scholar.jsp" target="_blank" className="text-blue-600 hover:underline p-0!">this page</Link>.
+            <br />
+            In case of any queries, please send email on <Link to="mailto:acrscholarship@iitb.ac.in" target="_blank" className="text-blue-600 hover:underline p-0!">acrscholarship@iitb.ac.in</Link>.
+        </p>
 
-       <h2 className="text-xl font-bold mb-2">IT Support Scholarship</h2>
-       <p className="mb-4">
-        The Director, IIT Bombay in collaboration with the generous alumni of IIT Bombay and Dean ACR office has come up with a financial support scheme for the economically weak students. This particular financial scheme finances the students to buy a laptop/personal computer and also provides students with a monthly data pack charge.
-       </p>
-       <h2 className="text-xl font-bold mb-2">
-        Loans available
-       </h2>
-       <h2 className="text-lg font-semibold mb-2">Scholar's loan (by SBI)</h2>
-       <p className="mb-4">
-        Does not require mortgage or security
-Loan can be in the name of the student or jointly with parents 
-The interest rate will probably be 6.65% this year.
-All the information for SBI Loan can be found.
-       </p>
-       <h2 className="text-lg font-semibold mb-2">
-        Vidyalaxmi loans
-       </h2>
-       <p className="mb-4">
-        Family income should be less than 9 lpa
-The interest rate can be expected to be about 8.5% pa and sophies can also apply from this year at about 9.24%.
-       </p>
-       <p className="mb-4">
-        Only the interest on the tuition fee, i.e 1 lakh per semester, is paid by IIT-B up till graduation. If he/she doesn't get placed after graduation, the student gets a year grace period. The loan has to be taken jointly with parents names
-Income papers are to be submitted to the academic office in the Main building.
-13 people applied last year and the reimbursement of money is still pending.
-       </p>
-       <h2 className="text-lg font-semibold mb-2">SBF (Student Benevolent Fund)</h2>
-       <p className="mb-4">
-        Family income certificate is required, and mostly given to the students in the second semester (since no CPI can be associated with students in the first semester)
-       </p>
-       <p className="mb-4">
-        While exceptional cases may be approved, family income should be lesser than 6.5 lpa. (This particular point is only for information, not to be published)
-       </p>
-       <p className="mb-4">
-        Tuition and the rest of the fees, along with mess advance, included.
-        Bad performance of a student may lead the Dean to cancel the scholarship. 4% interest rate (which might be increased).
-       </p>
-       <p className="mb-4">
-        If the student has signed an agreement to repay after he/she gets placed; but later opts for more time, he/she has to inform the office.
-       </p>
-       <h2 className="text-lg font-semibold mb-2">AP (by IITB-AA)</h2>
-       <p className="mb-4">
-        Financial Aid Programme* - 0% interest rate - By Tata Motors
-For UGs and PGs whose annual family income falls below 6.5 lpa, given the student has a cpi &gt; 6.
-       </p>
-       <p className="mb-4">
-        The student can contact the Alumni office through an online portal where they would be given the appropriate link to apply. Documents required for application - 12th Marksheets, JEE advanced results, Family income certificate, Parents Residential proof, Parents ID proof.
-This is followed by an interview with the FAP committee ( IIT-B Alumni )
-       </p>
-       <p className="mb-4">
-        Various kinds of assistance provided include - Tuition and mess fee; Purchasing a laptop; Internship expenses.
-The repayment is done after placement, with a grace period of 6 months.
-The above can be done by the PGs from their stipends.
-       </p>
-       <p className="mb-4">
-        <b>For further information: </b>Contact IIT Bombay Alumni Association Alumni Centre, 1st Floor Gulmohar Building, IIT Bombay<br /> 
-        <b>Email: </b><Link to="mailto:rama.iitbaa@gmail.com" target="_blank" className="text-blue-600 hover:underline p-0!">rama.iitbaa@gmail.com</Link><br /> <b>Tel: </b>022-25767086/7019
-       </p>
+        <h2 className="text-xl font-bold mb-2">IT Support Scholarship</h2>
+        <p className="mb-4">
+            The Director, IIT Bombay in collaboration with the generous alumni of IIT Bombay and Dean ACR office has come up with a financial support scheme for the economically weak students. This particular financial scheme finances the students to buy a laptop/personal computer and also provides students with a monthly data pack charge.
+        </p>
+        <h2 className="text-xl font-bold mb-2">
+            Loans available
+        </h2>
+        <h2 className="text-lg font-semibold mb-2">Scholar's loan (by SBI)</h2>
+        <p className="mb-4">
+            State Bank of India has exclusive education loan scheme for IIT Bombay Students named "SBI SCHOLAR LOAN". For more details please visit SBI bank branch located inside the campus close to Gymkhana and for features and other details refer the link: <Link to="https://sbi.co.in/web/personal-banking/loans/education-loans/student-loan-scheme" className="text-blue-600 hover:underline p-0!" target="_blank">https://sbi.co.in/web/personal-banking/loans/education-loans/student-loan-scheme</Link>
+        </p>
+        <h2 className="text-lg font-semibold mb-2">
+            Vidyalaxmi loans
+        </h2>
+        <p className="mb-4">
+            Students from any income group are eligible for the loan, but those with family income up to ₹8 lakh can receive a 3% interest subvention during the moratorium period (course duration + 1 year). For families with income up to ₹4.5 lakh, full interest subvention may be available under related government schemes.
+        </p>
+        <p className="mb-4">
+            The total loan amount depends on actual course and living expenses, with no upper limit. IIT Bombay supports students by reimbursing interest on tuition fee (INR 1 lakh per semester) till graduation, with a 1-year grace period if placement is not secured. The typical interest rate charged is around 8.5% per annum. The loan is taken jointly with parents, and income documents need to be submitted to the Academic Office.
+        </p>
+        <p className="mb-4">
+            For more information refer to: <Link to="https://www.education.gov.in/sites/upload_files/mhrd/files/document-reports/PM_Vidyalaxmi_Scheme_Guidelines.pdf" className="text-red-600 hover:underline p-0!" target="_blank"><FontAwesomeIcon icon={faFilePdf} size="lg"></FontAwesomeIcon></Link>
+        </p>
+        <h2 className="text-lg font-semibold mb-2">SBF (Student Benevolent Fund)</h2>
+        <p className="mb-4">
+            The Student Benevolent Fund at IIT Bombay supports enrolled students facing sudden financial hardship—whether due to medical emergencies, family crises, or other unexpected difficulties. Eligibility generally requires submission of family income documents (verified up to ₹6.5 lakh per annum) and fulfillment of academic criteria, such as maintaining satisfactory performance after the first semester.
+        </p>
+        <p className="mb-4">
+            Unlike a loan, this is a grant-based fund, so there's no interest charged. Disbursements typically cover urgent educational or living-related expenses like tuition, mess advance, hostel fees, or essential medical costs. Exceptional cases and need-based approvals are decided by the Dean’s office.
+        </p>
+        {/* <p className="mb-4">
+            Tuition and the rest of the fees, along with mess advance, included.
+            Bad performance of a student may lead the Dean to cancel the scholarship. 4% interest rate (which might be increased).
+        </p>
+        <p className="mb-4">
+            If the student has signed an agreement to repay after he/she gets placed; but later opts for more time, he/she has to inform the office.
+        </p> */}
+        <h2 className="text-lg font-semibold mb-2">FAP (by IITB-AA)</h2>
+        <p className="mb-4">
+            Financial Aid Programme* - 0% interest rate - By Tata Motors
+            For UGs and PGs whose annual family income falls below INR 7.5 lpa for metro-based candidates and INR 7 lakhs for non-metro-based candidates, given the student has a cpi &gt; 6.
+        </p>
+        <p className="mb-4">
+            The student can contact the Alumni office through an online portal where they would be given the appropriate link to apply. Documents required for application - 12th Marksheets, JEE advanced results, Family income certificate, Parents Residential proof, Parents ID proof.
+            This is followed by an interview with the FAP committee ( IIT-B Alumni )
+        </p>
+        <p className="mb-4">
+            Various kinds of assistance provided include - Tuition and mess fee; Purchasing a laptop; Internship expenses.
+            The repayment is done after placement, with a grace period of 6 months.
+            The above can be done by the PGs from their stipends.
+        </p>
+        <p className="mb-4">
+            <b>For further information: </b>Contact IIT Bombay Alumni Association Alumni Centre, 1st Floor Gulmohar Building, IIT Bombay<br />
+            <b>Website: </b><Link to="https://fap-iitbaa.org/Webcontent/FAQ" className="text-blue-600 hover:underline p-0!" target="_blank">https://fap-iitbaa.org/Webcontent/FAQ</Link><br />
+            <b>Email: </b><Link to="mailto:rama.iitbaa@gmail.com" target="_blank" className="text-blue-600 hover:underline p-0!">rama.iitbaa@gmail.com</Link><br /> <b>Tel: </b>022-25767086/7019
+        </p>
     </>);
     const sidebar = navData["Incoming Students"];
 
     return (
-        <Page content={content} sidebar={sidebar} sideHeading="Incoming Students"/>
+        <Page content={content} sidebar={sidebar} sideHeading="Incoming Students" />
     )
 }
 
