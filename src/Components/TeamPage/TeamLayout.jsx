4228487af6b2651ac6da01291552b9ec58112e74
Abdiitb <aryan223653badkul@gmail.com>
2025-06-22 01:24:03 +0530

    team page images added

--- src/Components/TeamPage/TeamLayout.jsx
+++ src/Components/TeamPage/TeamLayout.jsx
@@ -10,7 +10,7 @@ function TeamLayout() {
                     The Student Mentor Program (SMP) is a comprehensive institute body consisting of multiple verticals such as DAMP, ELP, and CTO, supported by a large team. It is led by the Overall Coordinators at the institute level, with Cabinet Heads overseeing each vertical. The Department Academic Mentorship Program (DAMP) functions under the guidance of 20 Departmental DAMP Coordinators and DAMP Cabinet Heads. The ELP and CTO verticals are also actively managed by their respective cabinet heads. In total, the SMP team comprises 144 Institute Student Mentors and 316 Department Academic Mentors, forming a strong support system for students across various departments and academic years.
                 </p>
             </div>
-            <div  className="md:text-xl text-md text-center italic mb-5">
+            <div  className="md:text-lg text-sm text-center italic font-bold mb-5">
                 Click or hover over image to know more about our team members
             </div>
             <TeamCarousel teamData={teamData}/>
