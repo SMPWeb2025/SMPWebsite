66b255c7234d6ffcca273110ba526ca438d77434
Abdiitb <aryan223653badkul@gmail.com>
2025-06-22 16:13:53 +0530

    Remove unused CSS files and assets

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -122,11 +122,6 @@ function Footer() {
                     Media
                   </Link>
                 </li>
-                {/* <li className="nav-item mb-2">
-                  <Link to="/extra-curriculars/sports" className="nav-link p-0 ">
-                    Sports
-                  </Link>
-                </li> */}
                 <li className="nav-item mb-2">
                   <Link to="/extra-curriculars/institute-bodies" className="nav-link p-0 ">
                     Institute Bodies
