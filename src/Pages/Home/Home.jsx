7b267ba50105fbddd437efa261aa6f3cf550f9d8
IronFist396 <atharvroday396@gmail.com>
2025-07-13 21:35:14 +0530

    updated seo

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
       <Helmet>
           <meta
           name="description"
-          content="Welcome to SMP IIT Bombay!"
+          content="Welcome to the website of Student Mentor Program, IIT Bombay! The main aim of this website is to acquaint you with the different facets of this college which you might be curious about. We hope this will help in making the difficult choice ahead of you and give you some insight into the culture at IIT Bombay from a student’s perspective!"
         />
         <meta
           name="keywords"
